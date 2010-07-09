7525e9da09ea401a6519a1787fd8d625e7c5aee9
Michael Stephens <me@mikej.st>
2010-07-09 17:20:52 -0400

    add an ignore_case keyword arg to jaro_winkler and improve its docstring

--- strfrymodule.c
+++ strfrymodule.c
@@ -1,23 +1,29 @@
 #include <Python.h>
 #include "jaro.h"
 
-static PyObject * strfry_jaro_winkler(PyObject *self, PyObject *args)
+static PyObject * strfry_jaro_winkler(PyObject *self, PyObject *args,
+    PyObject *keywds)
 {
     const char *s1, *s2;
     double result;
+    char ignore_case = 1;
 
-    if (!PyArg_ParseTuple(args, "ss", &s1, &s2)) {
+    static char *kwlist[] = {"string1", "string2", "ignore_case", NULL};
+
+    if (!PyArg_ParseTupleAndKeywords(args, keywds, "ss|B", kwlist, &s1, &s2,
+                                     &ignore_case)) {
         return NULL;
     }
 
-    result = jaro_winkler(s1, s2, 0, 1);
+    result = jaro_winkler(s1, s2, 0, ignore_case);
 
     return Py_BuildValue("d", result);
 }
 
 static PyMethodDef StrfryMethods[] = {
-    {"jaro_winkler", strfry_jaro_winkler, METH_VARARGS,
-     "Do a Jaro-Winkler string comparison."},
+    {"jaro_winkler", strfry_jaro_winkler, METH_VARARGS | METH_KEYWORDS,
+     "jaro_winkler(string1, string2, ignore_case=True)\n\n"
+     "Do a Jaro-Winkler string comparison between string1 and string2."},
 
     {NULL, NULL, 0, NULL}
 };
