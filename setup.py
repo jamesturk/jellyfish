1e08999ef18b5080c4db3387462e026b45412401
Peter Scott <peter@greplin.com>
2012-01-25 15:26:32 -0800

    Minor version bump.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, Extension
 long_description = open('README.rst').read()
 
 setup(name="jellyfish",
-      version="0.1.2",
+      version="0.1.3",
       platforms=["any"],
       description=("a library for doing approximate and "
                    "phonetic matching of strings."),
