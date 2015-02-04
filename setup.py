98171769076a9d254e13acff7fafdb06a922671b
James Turk <james.p.turk@gmail.com>
2015-02-04 12:22:16 -0500

    0.3.4 bump

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ def run_setup(build_c):
         long_description = readme.read()
 
     setup(name="jellyfish",
-          version="0.3.3",
+          version="0.3.4",
           platforms=["any"],
           description=("a library for doing approximate and "
                        "phonetic matching of strings."),
