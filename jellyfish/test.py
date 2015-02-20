30b9012017bb52d729de61355f0602e080d2166e
James Turk <james.p.turk@gmail.com>
2015-02-20 18:35:04 -0500

    all testdata separated

--- jellyfish/test.py
+++ jellyfish/test.py
@@ -53,19 +53,10 @@ class JellyfishTests(object):
             self.assertEqual(self.jf.damerau_levenshtein_distance(s1, s2), value)
 
     def test_soundex(self):
-        cases = [("Washington", "W252"),
-                 ("Lee", "L000"),
-                 ("Gutierrez", "G362"),
-                 ("Pfister", "P236"),
-                 ("Jackson", "J250"),
-                 ("Tymczak", "T522"),
-                 ("", ""),
-                 ("A", "A000"),
-                 (u"Çáŕẗéř", "C636"),
-                 ]
-
-        for (s1, code) in cases:
-            self.assertEqual(self.jf.soundex(s1), code)
+        with open('testdata/soundex.csv') as f:
+            data = csv.reader(f)
+            for (s1, code) in data:
+                self.assertEqual(self.jf.soundex(s1), code)
 
     def test_metaphone(self):
         with open('testdata/metaphone.csv') as f:
@@ -86,28 +77,23 @@ class JellyfishTests(object):
                 self.assertEqual(self.jf.match_rating_codex(s1), s2)
 
     def test_match_rating_comparison(self):
-        cases = [("Bryne", "Boern", True),
-                 ("Smith", "Smyth", True),
-                 ("Catherine", "Kathryn", True),
-                 ("Michael", "Mike", False),
-                 ("Tim", "Timothy", None),
-                 ("Ed", "Ad", True),
-                 ]
-
-        for (s1, s2, value) in cases:
-            self.assertEqual(self.jf.match_rating_comparison(s1, s2), value)
-
-    def test_match_rating_comparison_segfault(self):
-        import hashlib
-        sha1s = [hashlib.sha1(str(v).encode('ascii')).hexdigest() for v in range(100)]
-        # this segfaulted on 0.1.2
-        assert [[self.jf.match_rating_comparison(h1, h2) for h1 in sha1s] for h2 in sha1s]
+        with open('testdata/match_rating_comparison.csv') as f:
+            data = csv.reader(f)
+            for (s1, s2, value) in data:
+                value = {'True': True, 'False': False, 'None': None}[value]
+                self.assertEqual(self.jf.match_rating_comparison(s1, s2), value)
 
     def test_porter_stem(self):
         with open('testdata/porter.csv') as f:
             reader = csv.reader(f)
             for (a, b) in reader:
                 self.assertEqual(self.jf.porter_stem(a.lower()), b.lower())
+
+    def test_match_rating_comparison_segfault(self):
+        import hashlib
+        sha1s = [hashlib.sha1(str(v).encode('ascii')).hexdigest() for v in range(100)]
+        # this segfaulted on 0.1.2
+        assert [[self.jf.match_rating_comparison(h1, h2) for h1 in sha1s] for h2 in sha1s]
 
 
 class PyJellyfishTestCase(unittest.TestCase, JellyfishTests):
