3cb8fa6372785df1c91717a0d83b27ac3c26cbcc
James Turk <james.p.turk@gmail.com>
2015-04-21 17:14:58 -0400

    benchmarking added

--- jellyfish/test.py
+++ jellyfish/test.py
@@ -6,102 +6,145 @@ else:
     import csv
 import unittest
 import platform
+import pytest
 
 
-class JellyfishTests(object):
+def assertAlmostEqual(a, b, places=3):
+    assert abs(a - b) < (0.1**places)
 
-    def test_jaro_winkler(self):
+
+if platform.python_implementation() == 'CPython':
+    jf_params = ['python', 'c']
+else:
+    jf_params = ['python']
+
+
+@pytest.fixture(params=jf_params)
+def jf(request):
+    if request.param == 'python':
+        from jellyfish import _jellyfish as jf
+    else:
+        from jellyfish import cjellyfish as jf
+    return jf
+
+
+def test_jaro_winkler(jf, benchmark):
+    @benchmark
+    def _():
         with open('testdata/jaro_winkler.csv') as f:
             data = csv.reader(f)
             for (s1, s2, value) in data:
                 value = float(value)
-                self.assertAlmostEqual(self.jf.jaro_winkler(s1, s2), value, places=3)
+                assertAlmostEqual(jf.jaro_winkler(s1, s2), value, places=3)
 
-    def test_jaro_distance(self):
+
+def test_jaro_distance(jf, benchmark):
+    @benchmark
+    def _():
         with open('testdata/jaro_distance.csv') as f:
             data = csv.reader(f)
             for (s1, s2, value) in data:
                 value = float(value)
-                self.assertAlmostEqual(self.jf.jaro_distance(s1, s2), value, places=3)
+                assertAlmostEqual(jf.jaro_distance(s1, s2), value, places=3)
 
-    def test_hamming_distance(self):
+
+def test_hamming_distance(jf, benchmark):
+    @benchmark
+    def _():
         with open('testdata/hamming.csv') as f:
             data = csv.reader(f)
             for (s1, s2, value) in data:
                 value = int(value)
-                assert self.jf.hamming_distance(s1, s2) == value
+                assert jf.hamming_distance(s1, s2) == value
 
-    def test_levenshtein_distance(self):
+
+def test_levenshtein_distance(jf, benchmark):
+    @benchmark
+    def _():
         with open('testdata/levenshtein.csv') as f:
             data = csv.reader(f)
             for (s1, s2, value) in data:
                 value = int(value)
-                assert self.jf.levenshtein_distance(s1, s2) == value
+                assert jf.levenshtein_distance(s1, s2) == value
 
-    def test_damerau_levenshtein_distance(self):
+
+def test_damerau_levenshtein_distance(jf, benchmark):
+    @benchmark
+    def _():
         with open('testdata/damerau_levenshtein.csv') as f:
             data = csv.reader(f)
             for (s1, s2, value) in data:
                 value = int(value)
-                assert self.jf.damerau_levenshtein_distance(s1, s2) == value
+                assert jf.damerau_levenshtein_distance(s1, s2) == value
 
-    def test_soundex(self):
+
+def test_soundex(jf, benchmark):
+    @benchmark
+    def _():
         with open('testdata/soundex.csv') as f:
             data = csv.reader(f)
             for (s1, code) in data:
-                assert self.jf.soundex(s1) == code
+                assert jf.soundex(s1) == code
 
-    def test_metaphone(self):
+
+def test_metaphone(jf, benchmark):
+    @benchmark
+    def _():
         with open('testdata/metaphone.csv') as f:
             data = csv.reader(f)
             for (s1, code) in data:
-                assert self.jf.metaphone(s1) == code
+                assert jf.metaphone(s1) == code
 
-    def test_nysiis(self):
+
+def test_nysiis(jf, benchmark):
+    @benchmark
+    def _():
         with open('testdata/nysiis.csv') as f:
             data = csv.reader(f)
             for (s1, s2) in data:
-                assert self.jf.nysiis(s1) == s2
+                assert jf.nysiis(s1) == s2
 
-    def test_match_rating_codex(self):
+
+def test_match_rating_codex(jf, benchmark):
+    @benchmark
+    def _():
         with open('testdata/match_rating_codex.csv') as f:
             data = csv.reader(f)
             for (s1, s2) in data:
-                assert self.jf.match_rating_codex(s1) == s2
+                assert jf.match_rating_codex(s1) == s2
 
-    def test_match_rating_comparison(self):
+
+def test_match_rating_comparison(jf, benchmark):
+    @benchmark
+    def _():
         with open('testdata/match_rating_comparison.csv') as f:
             data = csv.reader(f)
             for (s1, s2, value) in data:
                 value = {'True': True, 'False': False, 'None': None}[value]
-                assert self.jf.match_rating_comparison(s1, s2) is value
+                assert jf.match_rating_comparison(s1, s2) is value
 
-    def test_porter_stem(self):
+
+def test_porter_stem(jf, benchmark):
+    @benchmark
+    def _():
         with open('testdata/porter.csv') as f:
             reader = csv.reader(f)
             for (a, b) in reader:
-                assert self.jf.porter_stem(a) == b
-
-    def test_match_rating_comparison_segfault(self):
-        import hashlib
-        sha1s = [hashlib.sha1(str(v).encode('ascii')).hexdigest() for v in range(100)]
-        # this segfaulted on 0.1.2
-        assert [[self.jf.match_rating_comparison(h1, h2) for h1 in sha1s] for h2 in sha1s]
-
-
-
-class PyJellyfishTestCase(unittest.TestCase, JellyfishTests):
-    from jellyfish import _jellyfish as jf      # noqa
+                assert jf.porter_stem(a) == b
 
 
 if platform.python_implementation() == 'CPython':
-    class CJellyfishTestCase(unittest.TestCase, JellyfishTests):
-        from jellyfish import cjellyfish as jf  # noqa
+    def test_match_rating_comparison_segfault():
+        import hashlib
+        from jellyfish import cjellyfish as jf
+        sha1s = [hashlib.sha1(str(v).encode('ascii')).hexdigest() for v in range(100)]
+        # this segfaulted on 0.1.2
+        assert [[jf.match_rating_comparison(h1, h2) for h1 in sha1s] for h2 in sha1s]
 
-        def test_damerau_levenshtein_distance_type(self):
-            self.jf.damerau_levenshtein_distance(u'abc', u'abc')
-            with self.assertRaisesRegexp(ValueError, 'requires unicode'):
-                self.jf.damerau_levenshtein_distance(b'abc', b'abc')
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_damerau_levenshtein_distance_type():
+        from jellyfish import cjellyfish as jf
+        jf.damerau_levenshtein_distance(u'abc', u'abc')
+        with pytest.raises(ValueError) as exc:
+            jf.damerau_levenshtein_distance(b'abc', b'abc')
+        assert 'requires unicode' in str(exc.value)
