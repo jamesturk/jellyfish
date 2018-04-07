6e8c44627e33e010a6e22d15a559bf9d90abd2c9
James Turk <james.p.turk@gmail.com>
2018-04-07 09:53:07 -0400

    add 3.6 to classifiers

--- setup.py
+++ setup.py
@@ -109,6 +109,7 @@ def run_setup(build_c):
                        "Programming Language :: Python :: 2.7",
                        "Programming Language :: Python :: 3.4",
                        "Programming Language :: Python :: 3.5",
+                       "Programming Language :: Python :: 3.6",
                        "Topic :: Text Processing :: Linguistic"],
           **kw)
 
