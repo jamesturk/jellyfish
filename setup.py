a3ae84074ea2f1b967a78da02809daab794fd877
James Turk <james.p.turk@gmail.com>
2018-04-16 13:03:16 -0400

    drop Py2 from testing / metadata

--- setup.py
+++ setup.py
@@ -106,7 +106,6 @@ def run_setup(build_c):
                        "License :: OSI Approved :: BSD License",
                        "Natural Language :: English",
                        "Operating System :: OS Independent",
-                       "Programming Language :: Python :: 2.7",
                        "Programming Language :: Python :: 3.4",
                        "Programming Language :: Python :: 3.5",
                        "Programming Language :: Python :: 3.6",
