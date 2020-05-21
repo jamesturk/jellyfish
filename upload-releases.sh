fa44b1b3269be13f568f06b9ccefc60b57a148c9
James Turk <dev@jamesturk.net>
2020-05-21 16:46:18 -0400

    fix some build files

--- upload-releases.sh
+++ upload-releases.sh
@@ -2,8 +2,9 @@
 set -ex
 export DOCKER_IMAGE=quay.io/pypa/manylinux2014_x86_64 
 export PLAT=manylinux2014_x86_64
-#docker run --rm -e PLAT=$PLAT -v `pwd`:/io $DOCKER_IMAGE /io/build-wheels.sh
 
+docker run --rm -e PLAT=$PLAT -v `pwd`:/io $DOCKER_IMAGE /io/build-wheels.sh
 python3 setup.py sdist
+
 twine upload dist/* wheelhouse/*manylinux*
 
