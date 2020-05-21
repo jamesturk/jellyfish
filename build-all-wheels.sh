96349141ae70729d1d0b98ce5f0564ff35d77636
James Turk <dev@jamesturk.net>
2020-05-21 16:38:18 -0400

    upload releases script

--- build-all-wheels.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-export DOCKER_IMAGE=quay.io/pypa/manylinux2014_x86_64 
-export PLAT=manylinux2014_x86_64
-docker run --rm -e PLAT=$PLAT -v `pwd`:/io $DOCKER_IMAGE /io/build-wheels.sh
-
-twine upload wheelhouse/*manylinux*
