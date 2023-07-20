174542b3132c0b6f266049fa2e7d95545ab32cab
James Turk <dev@jamesturk.net>
2023-07-20 13:47:14 -0500

    actually remove deprecated aliases

--- python/jellyfish/__init__.py
+++ python/jellyfish/__init__.py
@@ -2,21 +2,3 @@ import warnings
 
 from ._rustyfish import *
 from . import _jellyfish
-
-
-def jaro_winkler(s1, s2, long_tolerance=False):
-    warnings.warn(
-        "the name 'jaro_winkler' is deprecated and will be removed in jellyfish 1.0, "
-        "for the same functionality please use jaro_winkler_similarity",
-        DeprecationWarning,
-    )
-    return jaro_winkler_similarity(s1, s2, long_tolerance)  # noqa
-
-
-def jaro_distance(s1, s2):
-    warnings.warn(
-        "the jaro_distance function incorrectly returns the jaro similarity, "
-        "replace your usage with jaro_similarity before 1.0",
-        DeprecationWarning,
-    )
-    return jaro_similarity(s1, s2)  # noqa
--- tests/test_jellyfish.py
+++ tests/test_jellyfish.py
@@ -39,22 +39,6 @@ def test_jaro_winkler_similarity_longtol(jf, s1, s2, value):
     assertAlmostEqual(jf.jaro_winkler_similarity(s1, s2, True), value, places=3)
 
 
-def test_jaro_winkler_deprecation(jf):
-    # backwards compatibility function
-    from jellyfish import jaro_winkler
-
-    with pytest.deprecated_call():
-        assert jaro_winkler("a", "a") == 1
-
-
-def test_jaro_distance_deprecation():
-    # backwards compatibility function
-    from jellyfish import jaro_distance
-
-    with pytest.deprecated_call():
-        assert jaro_distance("a", "a") == 1
-
-
 @pytest.mark.parametrize("s1,s2,value", _load_data("jaro_distance"), ids=str)
 def test_jaro_similarity(jf, s1, s2, value):
     value = float(value)
